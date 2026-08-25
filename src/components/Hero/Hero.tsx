3385e292e3459f3afa5ffd31869939b03af38bdb
vishalsukhwall <vishalsukhwal33@gmail.com>
2026-08-25 23:49:55 +0530

    new changes in about.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,23 +4,6 @@ import HeroContent from './HeroContent';
 import HeroCanvas from './HeroCanvas';
 import ScrollIndicator from './ScrollIndicator';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const Hero: React.FC = () => {
   return (
     <section 
